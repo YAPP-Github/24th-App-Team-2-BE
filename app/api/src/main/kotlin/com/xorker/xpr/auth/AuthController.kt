3c462e914c94fc90f57f9a95ae7f5a96bfd8e1d1
sunwoong <sunwoong1122@gmail.com>
2024-06-06 18:32:33 +0900

    XPR-7 feat: auth controller 구현

---
+++ app/api/src/main/kotlin/com/xorker/xpr/auth/AuthController.kt
@@ -0,0 +1,35 @@
+package com.xorker.xpr.auth
+
+import com.xorker.xpr.auth.dto.AuthReissueRequest
+import com.xorker.xpr.auth.dto.AuthSignInRequest
+import com.xorker.xpr.auth.dto.AuthTokenResponse
+import com.xorker.xpr.auth.dto.toResponse
+import com.xorker.xpr.support.auth.NeedLogin
+import com.xorker.xpr.support.auth.PrincipalUser
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthController(
+    private val authUserCase: AuthUseCase
+) {
+    @PostMapping("/api/v1/auth/signin")
+    fun signIn(@RequestBody request: AuthSignInRequest): AuthTokenResponse {
+        val token = authUserCase.signIn(request.authType, request.token)
+        return token.toResponse()
+    }
+
+    @PostMapping("/api/v1/auth/reissue")
+    fun reissue(@RequestBody request: AuthReissueRequest): AuthTokenResponse {
+        val token = authUserCase.reissue(request.refreshToken)
+        return token.toResponse()
+    }
+
+    @NeedLogin
+    @DeleteMapping("/api/v1/auth/withdrawal")
+    fun withdrawal(principalUser: PrincipalUser) {
+        authUserCase.withdrawal(principalUser.userId)
+    }
+}

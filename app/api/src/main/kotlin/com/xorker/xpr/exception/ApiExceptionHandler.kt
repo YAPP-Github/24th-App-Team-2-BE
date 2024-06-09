ae2644e510d568e749b358155252416f8b2215fa
sunwoong <sunwoong1122@gmail.com>
2024-06-09 21:41:03 +0900

    XPR-8 feat: @ControllerAdvice 어노테이션 추가

--- app/api/src/main/kotlin/com/xorker/xpr/exception/ApiExceptionHandler.kt
+++ app/api/src/main/kotlin/com/xorker/xpr/exception/ApiExceptionHandler.kt
@@ -4,16 +4,16 @@ import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Component
 import org.springframework.validation.BindException
 import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 typealias ExceptionResponseEntity = ResponseEntity<ExceptionResponse>
 
-@Component
+@ControllerAdvice
 class ApiExceptionHandler(
     private val responseFactory: ExceptionResponseFactory,
 ) {

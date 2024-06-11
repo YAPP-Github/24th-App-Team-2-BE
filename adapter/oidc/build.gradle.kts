e9c3c80e093b08365f30ae3f0ad637f03496bbf6
sunwoong <sunwoong1122@gmail.com>
2024-06-11 19:50:17 +0900

    XPR-39 chore: oidc adapter 의존성 설정

---
+++ adapter/oidc/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    val springBootVersion by properties
+    implementation(project(":domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter:$springBootVersion")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+}
+
+tasks {
+    withType<Jar> { enabled = true }
+    withType<BootJar> { enabled = false }
+}

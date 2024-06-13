547931f936222c02b42a4d8ee238b1c8f78a7f92
sunwoong <sunwoong1122@gmail.com>
2024-06-13 20:18:35 +0900

    XPR-55 feat: security filter jwt validate 로직 구현

--- app/support/auth/src/main/kotlin/com/xorker/xpr/support/auth/core/JwtService.kt
+++ app/support/auth/src/main/kotlin/com/xorker/xpr/support/auth/core/JwtService.kt
@@ -1,12 +1,21 @@
 package com.xorker.xpr.support.auth.core
 
+import com.xorker.xpr.support.jwt.JwtProvider
+import com.xorker.xpr.support.jwt.JwtSecretKey
 import com.xorker.xpr.user.UserId
 import org.springframework.stereotype.Service
 
 @Service
 internal class JwtService : TokenUseCase {
+    private val jwtProvider = JwtProvider
+
     override fun getUserId(token: String): UserId {
-        // TODO
-        throw JwtValidationFailException
+        val isValidToken = jwtProvider.validate(token, JwtSecretKey)
+        if (!isValidToken) {
+            throw JwtValidationFailException
+        }
+        val subject = jwtProvider.getSubject(token, JwtSecretKey) ?: throw JwtValidationFailException
+
+        return UserId(subject.toLong())
     }
 }

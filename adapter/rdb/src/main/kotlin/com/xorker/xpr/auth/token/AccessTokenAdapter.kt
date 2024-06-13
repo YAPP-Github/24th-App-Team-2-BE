4f43eb5af897215588eb9d85acba3f4477c1b629
sunwoong <sunwoong1122@gmail.com>
2024-06-13 20:18:00 +0900

    XPR-55 feat: access token adapter 로직 구현

---
+++ adapter/rdb/src/main/kotlin/com/xorker/xpr/auth/token/AccessTokenAdapter.kt
@@ -0,0 +1,25 @@
+package com.xorker.xpr.auth.token
+
+import com.xorker.xpr.support.jwt.JwtProvider
+import com.xorker.xpr.support.jwt.JwtSecretKey
+import com.xorker.xpr.user.UserId
+import java.time.LocalDateTime
+import org.springframework.stereotype.Component
+
+@Component
+internal class AccessTokenAdapter : AccessTokenQueryRepository, AccessTokenCommandRepository {
+    private val jwtProvider = JwtProvider
+
+    override fun getUserIdOrThrow(accessToken: String): UserId {
+        return UserId(0L) // TODO 추후 메서드 필요 시 변경
+    }
+
+    override fun createAccessToken(userId: UserId): String {
+        val now = LocalDateTime.now()
+        return jwtProvider.generate(
+            id = userId.value.toString(),
+            expiration = now.plusMinutes(10),
+            key = JwtSecretKey,
+        )
+    }
+}

e2eceb80532e4a4697aea35f31d116c2f1465847
sunwoong <sunwoong1122@gmail.com>
2024-07-15 00:44:05 +0900

    DRAW-96 feat: 마피아 게임 messenger 로직 구현

--- app/websocket/src/main/kotlin/com/xorker/draw/websocket/message/response/MafiaGameMessengerImpl.kt
+++ app/websocket/src/main/kotlin/com/xorker/draw/websocket/message/response/MafiaGameMessengerImpl.kt
@@ -1,10 +1,16 @@
 package com.xorker.draw.websocket.message.response
 
+import com.xorker.draw.exception.InvalidGameStatusException
+import com.xorker.draw.mafia.MafiaGameInfo
 import com.xorker.draw.mafia.MafiaGameMessenger
+import com.xorker.draw.mafia.MafiaPhase
 import com.xorker.draw.mafia.MafiaPlayer
 import com.xorker.draw.room.Room
+import com.xorker.draw.websocket.BranchedBroadcastEvent
 import com.xorker.draw.websocket.BroadcastEvent
 import com.xorker.draw.websocket.broker.WebSocketBroadcaster
+import com.xorker.draw.websocket.message.response.dto.MafiaGameInfoBody
+import com.xorker.draw.websocket.message.response.dto.MafiaGameInfoMessage
 import com.xorker.draw.websocket.message.response.dto.MafiaPlayerListBody
 import com.xorker.draw.websocket.message.response.dto.MafiaPlayerListMessage
 import com.xorker.draw.websocket.message.response.dto.toResponse
@@ -14,6 +20,7 @@ import org.springframework.stereotype.Component
 class MafiaGameMessengerImpl(
     private val broadcaster: WebSocketBroadcaster,
 ) : MafiaGameMessenger {
+
     override fun broadcastPlayerList(room: Room<MafiaPlayer>) {
         val roomId = room.id
 
@@ -27,5 +34,47 @@ class MafiaGameMessengerImpl(
         val event = BroadcastEvent(roomId, message)
 
         broadcaster.publishBroadcastEvent(event)
+    }
+
+    override fun broadcastGameInfo(mafiaGameInfo: MafiaGameInfo) {
+        val roomId = mafiaGameInfo.room.id
+
+        val phase = mafiaGameInfo.phase as? MafiaPhase.Playing ?: throw InvalidGameStatusException
+
+        val mafia = phase.mafiaPlayer
+        val keyword = phase.keyword
+        val turnList = phase.turnList
+
+        val gameOption = mafiaGameInfo.gameOption
+
+        val message = MafiaGameInfoMessage(
+            MafiaGameInfoBody(
+                category = keyword.category,
+                answer = keyword.answer,
+                turnList = turnList.map { it.toResponse() }.toList(),
+                gameOption = gameOption.toResponse(),
+            ),
+        )
+
+        val branchedMessage = MafiaGameInfoMessage(
+            MafiaGameInfoBody(
+                isMafia = true,
+                category = keyword.category,
+                answer = keyword.answer,
+                turnList = turnList.map { it.toResponse() }.toList(),
+                gameOption = gameOption.toResponse(),
+            ),
+        )
+
+        val branched = setOf(mafia.userId)
+
+        val event = BranchedBroadcastEvent(
+            roomId = roomId,
+            branched = branched,
+            message = message,
+            branchedMessage = branchedMessage,
+        )
+
+        broadcaster.publishBranchedBroadcastEvent(event)
     }
 }

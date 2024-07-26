ebb9982918041bae91928d1ccac18cf2e5a32d23
HoYeon Lee <comforest@naver.com>
2024-07-26 17:28:57 +0900

    DRAW-157 fix: Timer에서 Coroutine Job 반환

--- adapter/memory/src/main/kotlin/com/xorker/draw/timer/TimerAdapter.kt
+++ adapter/memory/src/main/kotlin/com/xorker/draw/timer/TimerAdapter.kt
@@ -1,7 +1,9 @@
 package com.xorker.draw.timer
 
 import java.time.Duration
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.springframework.context.ApplicationEventPublisher
@@ -12,10 +14,12 @@ internal class TimerAdapter(
     private val eventPublisher: ApplicationEventPublisher,
 ) : TimerRepository {
 
-    override fun <T : Any> startTimer(interval: Duration, event: T) {
-        GlobalScope.launch {
+    override fun <T : Any> startTimer(interval: Duration, event: T): Job {
+        val job = Job()
+        CoroutineScope(job + Dispatchers.IO).launch {
             delay(interval.toMillis())
             eventPublisher.publishEvent(event)
         }
+        return job
     }
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,5 +1,9 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES}")
+}
+
 tasks {
     withType<Jar> { enabled = true }
     withType<BootJar> { enabled = false }

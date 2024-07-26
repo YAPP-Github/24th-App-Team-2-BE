b708634247af489678b12304876502568ec91d8b
HoYeon Lee <comforest@naver.com>
2024-07-26 18:27:57 +0900

    DRAW-158 feat: Unicast

--- app/websocket/src/main/kotlin/com/xorker/draw/websocket/broker/WebSocketBroadcaster.kt
+++ app/websocket/src/main/kotlin/com/xorker/draw/websocket/broker/WebSocketBroadcaster.kt
@@ -1,8 +1,12 @@
 package com.xorker.draw.websocket.broker
 
+import com.xorker.draw.room.RoomId
+import com.xorker.draw.user.UserId
 import com.xorker.draw.websocket.BranchedBroadcastEvent
 import com.xorker.draw.websocket.BroadcastEvent
 import com.xorker.draw.websocket.RespectiveBroadcastEvent
+import com.xorker.draw.websocket.SessionMessage
+import com.xorker.draw.websocket.UnicastEvent
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
 
@@ -10,6 +14,14 @@ import org.springframework.stereotype.Component
 class WebSocketBroadcaster(
     private val publisher: ApplicationEventPublisher,
 ) {
+    fun unicast(userId: UserId, message: SessionMessage) {
+        publisher.publishEvent(UnicastEvent(userId, message))
+    }
+
+    fun publishBroadcastEvent(roomId: RoomId, sessionMessage: SessionMessage) {
+        publisher.publishEvent(BroadcastEvent(roomId, sessionMessage))
+    }
+
     fun publishBroadcastEvent(event: BroadcastEvent) {
         publisher.publishEvent(event)
     }
--- core/src/main/kotlin/com/xorker/draw/websocket/SessionMessageBroker.kt
+++ core/src/main/kotlin/com/xorker/draw/websocket/SessionMessageBroker.kt
@@ -1,6 +1,7 @@
 package com.xorker.draw.websocket
 
 interface SessionMessageBroker {
+    fun unicast(event: UnicastEvent)
     fun broadcast(event: BroadcastEvent)
     fun broadcast(event: BranchedBroadcastEvent)
     fun broadcast(event: RespectiveBroadcastEvent)

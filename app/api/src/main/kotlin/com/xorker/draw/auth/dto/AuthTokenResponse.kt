4e4e50ee14158fb206a42b5b605b70b842dff6d1
sunwoong <sunwoong1122@gmail.com>
2024-07-29 16:17:50 +0900

    DRAW-179 feat: 로그인 응답값 userId 추가

--- app/api/src/main/kotlin/com/xorker/draw/auth/dto/AuthTokenResponse.kt
+++ app/api/src/main/kotlin/com/xorker/draw/auth/dto/AuthTokenResponse.kt
@@ -1,6 +1,7 @@
 package com.xorker.draw.auth.dto
 
 import com.xorker.draw.auth.token.Token
+import com.xorker.draw.user.UserId
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Auth 공통 응답 DTO 입니다.")
@@ -9,9 +10,12 @@ data class AuthTokenResponse(
     val accessToken: String,
     @Schema(description = "Refresh Token 입니다.")
     val refreshToken: String,
+    @Schema(description = "UserId 입니다.")
+    val userId: UserId,
 )
 
 fun Token.toResponse(): AuthTokenResponse = AuthTokenResponse(
     accessToken = this.accessToken,
     refreshToken = this.refreshToken,
+    userId = this.userId,
 )

ddf4fca693261b1d1e44ffd4c6c69d7b21cc078e
HoYeon Lee <comforest@naver.com>
2024-08-06 21:55:16 +0900

    DRAW-219 fix: 참여 할 때 색 랜덤 배정

--- core/src/main/kotlin/com/xorker/draw/mafia/MafiaGameRoomService.kt
+++ core/src/main/kotlin/com/xorker/draw/mafia/MafiaGameRoomService.kt
@@ -85,7 +85,9 @@ internal class MafiaGameRoomService(
         val alreadyUsedColors =
             gameInfo.room.players.map { it.color }.toList()
 
-        return COLOR_LIST.filterNot { alreadyUsedColors.contains(it) }
+        return COLOR_LIST
+            .filterNot { alreadyUsedColors.contains(it) }
+            .shuffled()
             .first()
     }
 

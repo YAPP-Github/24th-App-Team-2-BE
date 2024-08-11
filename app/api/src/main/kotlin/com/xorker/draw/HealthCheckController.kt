a84b6ce95f0deb808796ad421efd66a1f5a4ed15
HoYeon Lee <comforest@naver.com>
2024-08-11 22:00:25 +0900

    DRAW-192 feat: 강제 업데이트 Swagger

--- app/api/src/main/kotlin/com/xorker/draw/HealthCheckController.kt
+++ app/api/src/main/kotlin/com/xorker/draw/HealthCheckController.kt
@@ -2,19 +2,23 @@ package com.xorker.draw
 
 import com.xorker.draw.support.logging.logger
 import com.xorker.draw.version.ApiMinVersion
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.slf4j.MDC
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-
+@Tag(name = "개발용 API")
 @RestController
 class HealthCheckController {
     val log = logger()
 
+    @Operation(hidden = true)
     @GetMapping("/ping")
     fun ping(): String {
         return "pong"
     }
 
+    @Operation(hidden = true)
     @GetMapping("/test")
     fun test(): String {
         MDC.put("test", "test")
@@ -22,8 +26,8 @@ class HealthCheckController {
         return "pong"
     }
 
+    @Operation(summary = "강업 테스트")
     @ApiMinVersion(androidVersion = "99.99.99", iosVersion = "99.99.99")
     @GetMapping("/force-update")
-    fun forceUpdate() {
-    }
+    fun forceUpdate() {}
 }

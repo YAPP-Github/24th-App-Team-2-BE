84a6a23c3d6644db81a1c8e9938387e17e13defa
HoYeon Lee <comforest@naver.com>
2024-08-18 22:53:06 +0900

    DRAW-240 feat: API Logger

--- support/logging/src/main/kotlin/com/xorker/draw/support/logging/Logger.kt
+++ support/logging/src/main/kotlin/com/xorker/draw/support/logging/Logger.kt
@@ -1,6 +1,12 @@
 package com.xorker.draw.support.logging
 
+import java.util.UUID
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 
 inline fun <reified T> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
+
+fun registerRequestId() {
+    MDC.put("requestId", UUID.randomUUID().toString())
+}
--- support/logging/src/main/kotlin/com/xorker/draw/support/logging/XorkerJsonLayout.kt
+++ support/logging/src/main/kotlin/com/xorker/draw/support/logging/XorkerJsonLayout.kt
@@ -3,6 +3,7 @@ package com.xorker.draw.support.logging
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.contrib.jackson.JacksonJsonFormatter
 import ch.qos.logback.contrib.json.classic.JsonLayout
+import org.slf4j.MDC
 
 class XorkerJsonLayout : JsonLayout() {
 
@@ -21,5 +22,14 @@ class XorkerJsonLayout : JsonLayout() {
 
     override fun addCustomDataToJsonMap(map: MutableMap<String, Any>, event: ILoggingEvent) {
         super.addCustomDataToJsonMap(map, event)
+
+        MDC_LIST.forEach { key ->
+            MDC.get(key)?.let { value -> map[key] = value }
+        }
+    }
+
+    companion object {
+        // MDC에서 Json에 넣을 목록
+        private val MDC_LIST = listOf("requestId", "userId")
     }
 }

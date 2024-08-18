29fa57a084659ffb88c3b9a4c4644e4c6c72f7e3
HoYeon Lee <comforest@naver.com>
2024-08-18 15:04:30 +0900

    DRAW-240 fix: Console 로그 패턴 수정

--- support/logging/src/main/kotlin/com/xorker/draw/support/logging/XorkerJsonLayout.kt
+++ support/logging/src/main/kotlin/com/xorker/draw/support/logging/XorkerJsonLayout.kt
@@ -7,13 +7,16 @@ import ch.qos.logback.contrib.json.classic.JsonLayout
 class XorkerJsonLayout : JsonLayout() {
 
     init {
-        setTimestampFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+        setTimestampFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
         setTimestampFormatTimezoneId("Asia/Seoul")
         appendLineSeparator = true
 
         jsonFormatter = JacksonJsonFormatter().apply {
             this.isPrettyPrint = false
         }
+
+        includeMDC = false
+        includeContextName = false
     }
 
     override fun addCustomDataToJsonMap(map: MutableMap<String, Any>, event: ILoggingEvent) {

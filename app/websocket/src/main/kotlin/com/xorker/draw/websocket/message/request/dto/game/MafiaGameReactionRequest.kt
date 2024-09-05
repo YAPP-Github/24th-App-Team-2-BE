b57cd1444e01240d648e52a210cd113bb8a0ce57
sunwoong <sunwoong1122@gmail.com>
2024-09-05 17:53:50 +0900

    DRAW-298 feat: 리액션 dto 구현

---
+++ app/websocket/src/main/kotlin/com/xorker/draw/websocket/message/request/dto/game/MafiaGameReactionRequest.kt
@@ -0,0 +1,5 @@
+package com.xorker.draw.websocket.message.request.dto.game
+
+data class MafiaGameReactionRequest(
+    val reaction: String,
+)
---
+++ app/websocket/src/main/kotlin/com/xorker/draw/websocket/message/response/dto/game/MafiaGameReactionMessage.kt
@@ -0,0 +1,15 @@
+package com.xorker.draw.websocket.message.response.dto.game
+
+import com.xorker.draw.websocket.ResponseAction
+import com.xorker.draw.websocket.SessionMessage
+
+class MafiaGameReactionMessage(
+    override val body: MafiaGameReactionBody,
+) : SessionMessage {
+    override val action = ResponseAction.REACTION
+    override val status = SessionMessage.Status.OK
+}
+
+data class MafiaGameReactionBody(
+    val reaction: String,
+)

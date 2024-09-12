3c8ba0f3311d6570e1a8323f30b43396d14c3885
HoYeon Lee <comforest@naver.com>
2024-09-13 00:03:16 +0900

    DRAW-330 chore: Firebase 세팅

---
+++ adapter/firebase/build.gradle.kts
@@ -0,0 +1,19 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    kotlin("plugin.spring")
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":support:metric"))
+
+    implementation("org.springframework.boot:spring-boot-starter:${Versions.SPRING_BOOT}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES}")
+    implementation("com.google.firebase:firebase-admin:${Versions.FIREBASE}")
+}
+
+tasks {
+    withType<Jar> { enabled = true }
+    withType<BootJar> { enabled = false }
+}
